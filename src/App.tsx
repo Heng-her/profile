29e74b9af2c38234e9b5a268367e5145bde228b0
Her bunheng <hengnun68@gmail.com>
2025-11-29 12:59:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import PublicPage from "./page/Public";
 import About from "./page/Public/about";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./page/Dashboard/dashboard";
+import Login from "./page/auth/login";
+import Contact from "./page/Public/contact";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<PublicPage />} />
       <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<h1>Contact Page</h1>} />
-
+      <Route path="/login" element={<Login />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
       {/* 404 Fallback */}
       <Route path="*" element={<h1>Page Not Found</h1>} />
     </Routes>
